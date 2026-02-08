a6c4d4a01da5cab1434f39eb7d559c2b5a39d2b7
Muhammad Ubdale <99432158+Ubdale@users.noreply.github.com>
2026-02-08 18:44:05 +0500

    Adding Show Pass Functionality

--- app.js
+++ app.js
@@ -488,3 +488,19 @@ function displaySearchResults(movies) {
         `;
     }).join('');
 }
+
+// --- Password Visibility Toggle ---
+window.togglePasswordVisibility = function (inputId, toggleElement) {
+    const passwordInput = document.getElementById(inputId);
+    const icon = toggleElement.querySelector('i');
+
+    if (passwordInput.type === 'password') {
+        passwordInput.type = 'text';
+        icon.classList.remove('fa-eye');
+        icon.classList.add('fa-eye-slash');
+    } else {
+        passwordInput.type = 'password';
+        icon.classList.remove('fa-eye-slash');
+        icon.classList.add('fa-eye');
+    }
+};
